468b5e728366b2ad5e7efb1e5e06e2d7a29f604d
f.gotelip <felipegotdelga@gmail.com>
2024-09-10 10:58:21 -0300

    java util completo

--- Banco/src/main/java/org/example/Pacote/Conta.java
+++ Banco/src/main/java/org/example/Pacote/Conta.java
@@ -1,11 +1,7 @@
 package org.example.Pacote;
 import org.example.PJ;
 import org.example.CSalario;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public abstract class Conta {
     private int idConta;

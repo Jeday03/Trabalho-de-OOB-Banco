e908a66da2be0879608c63412b7e51a76f930b6a
Rafael <rafaelgawry@gmail.com>
2024-09-14 16:11:46 -0300

    Bug de Arquivo Sobrereescrevendo concertado

--- Banco/src/main/java/org/example/ProcessarArquivos.java
+++ Banco/src/main/java/org/example/ProcessarArquivos.java
@@ -5,30 +5,31 @@ import java.util.List;
 import org.example.Pacote.PJ;
 public class ProcessarArquivos {
     public static void salvarPessoas(String caminhoArquivo, List<Pessoa> pessoas) {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(caminhoArquivo))) {
-            for (Pessoa pessoa : pessoas) {
-                if(pessoa instanceof PF){
-                    PF pf = (PF) pessoa;
-                    bw.write("PF;" + pf.getNome() + ";" +
-                        pf.getTelefone() + ";" +
-                        pf.getEmail() + ";" +
-                        pf.getSenha() + ";" +
-                        pf.getCpf());
-                }else if (pessoa instanceof PJ) {
-                    PJ pj = (PJ) pessoa;
-                    bw.write("PJ;" + pj.getNome() + ";" +
-                            pj.getTelefone() + ";" +
-                            pj.getEmail() + ";" +
-                            pj.getSenha() + ";" +
-                            pj.getCnpj());
-                }
-                bw.newLine();
+    try (BufferedWriter bw = new BufferedWriter(new FileWriter(caminhoArquivo, true))) { // Adiciona 'true' para modo append
+        for (Pessoa pessoa : pessoas) {
+            if(pessoa instanceof PF){
+                PF pf = (PF) pessoa;
+                bw.write("PF;" + pf.getNome() + ";" +
+                    pf.getTelefone() + ";" +
+                    pf.getEmail() + ";" +
+                    pf.getSenha() + ";" +
+                    pf.getCpf());
+            }else if (pessoa instanceof PJ) {
+                PJ pj = (PJ) pessoa;
+                bw.write("PJ;" + pj.getNome() + ";" +
+                        pj.getTelefone() + ";" +
+                        pj.getEmail() + ";" +
+                        pj.getSenha() + ";" +
+                        pj.getCnpj());
             }
-            System.out.println("Dados de pessoas salvos com sucesso!");
-        } catch (IOException e) {
-            System.err.println("Erro ao salvar pessoas: " + e.getMessage());
+            bw.newLine();
         }
+        System.out.println("Dados de pessoas salvos com sucesso!");
+    } catch (IOException e) {
+        System.err.println("Erro ao salvar pessoas: " + e.getMessage());
     }
+}
+
     public static List<Pessoa> carregarPessoas(String caminhoArquivo) {
         List<Pessoa> pessoas = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo))) {

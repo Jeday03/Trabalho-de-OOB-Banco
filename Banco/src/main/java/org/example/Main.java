8dc5d07227c45051a7dc7ece97976b3d36fa4342
Rafael <rafaelgawry@gmail.com>
2024-09-14 12:54:35 -0300

    Agora ele le e cria e imprime o arquivo na main

--- Banco/src/main/java/org/example/Main.java
+++ Banco/src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 
+import java.util.ArrayList;
+import java.util.List;
 import org.example.Pacote.PJ;
 
 import javax.swing.JFrame;
@@ -9,6 +11,8 @@ import javax.swing.JFrame;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        List<Pessoa> listaPessoas = new ArrayList<>();
+        
         CSalario cClaudio = new CSalario();
         CPoupanca cFelipe = new CPoupanca();
         CCorrente cMarcos = new CCorrente();
@@ -16,10 +20,31 @@ public class Main {
 
         PF pf1 = new PF("Felipe","32938034","felipe@gmail.com","1234","rio branco 4065","123.456.789-01");
         PF pf2 = new PF("Claudio","83230223","claudio@gmail.com","1234","RIo branco","123.456.789-01");
-
+        listaPessoas.add(pf1);
+        listaPessoas.add(pf2);
 
         PJ pj1 =  new PJ("Marcos","329994895","marcos@gmail.com","1234","bom pastor","889370001");
-
+        listaPessoas.add(pj1);
+        
+        String caminhoArquivo = "pessoas.txt";
+        ProcessarArquivos.salvarPessoas(caminhoArquivo, listaPessoas);
+        
+        
+        List<Pessoa> pessoasCarregadas = ProcessarArquivos.carregarPessoas(caminhoArquivo);
+        
+        for (Pessoa pessoa : pessoasCarregadas) {
+            if (pessoa instanceof PF) {
+                PF pf = (PF) pessoa;
+                System.out.println("Pessoa Fisica: " + pf.getNome() + " - CPF: " + pf.getCpf());
+            } else if (pessoa instanceof PJ) {
+                PJ pj = (PJ) pessoa;
+                System.out.println("Pessoa Juridica: " + pj.getNome() + " - CNPJ: " + pj.getCnpj());
+            }
+        }
+        
+        
+/*
+        
         //vou implementar a inteface a partir daqui
 
         // Cria o frame
@@ -36,7 +61,7 @@ public class Main {
         LoginFrame.pack();
         LoginFrame.setVisible(true);
         LoginFrame.setLocationRelativeTo(null);
-
+*/
 
     }
 }
